0211b0cf65953f876837055ec3f25b1c9fd61d3b
Andres Montenegro <46237091+rootaj@users.noreply.github.com>
2020-09-01 11:31:36 -0500

    Actualizacion de la vista principal

--- vistas/modulos/inicio.php
+++ vistas/modulos/inicio.php
@@ -32,6 +32,253 @@
         </a>
       </div>
 
+
+      <div class="container-fluid mt-4">
+        <h2 class="text-primary text-center mb-4">Nuestras especialidades</h2>
+
+        <div class="row mb-4">
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/oftamologia.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Oftamlogia</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/ginecologia.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Ginecologia</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/otorrino.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Otorrinolaringología</span>
+            </a>
+          </div>
+        </div>
+
+        <div class="row mt-4">
+
+          <div class="col-4 ">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/neurologia.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Neurologia</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/urologia.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Urologia</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/oncologia.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Oncologia</span>
+            </a>
+          </div>
+        </div>
+
+        <div class="row mt-4">
+
+          <div class="col-4 ">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/hepatologia.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Hepatologia</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/cardiologia.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Cardiologia</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/gastro.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Gastroenterologia</span>
+            </a>
+          </div>
+        </div>
+
+        <div class="row mt-4">
+
+          <div class="col-4 ">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/odontologia.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Odontologia</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/pediatria.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Pediatria</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/obstetricia.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Obstetricia</span>
+            </a>
+          </div>
+        </div>
+
+
+        <div class="row mt-4">
+
+          <div class="col-4 ">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/traumatologia.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Traumatologia</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/laboratorio.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Laboratorio</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/covid.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Covid-19</span>
+            </a>
+          </div>
+        </div>
+
+
+      </div>
+
+      <!-- SECCION DE FRENA LA CURVA -->
+
+      <div class="container-fluid mt-4 mb-4">
+        <div class="row">
+          <div class="col-8">
+            <img src="vistas/img/frenaCurva.svg" class="mx-auto d-block" alt="">
+          </div>
+          <div class="col-4">
+            <img src="vistas/img/covid.svg" class="mx-auto d-block" alt="">
+          </div>
+        </div>
+      </div>
+
+
+      <!-- SECCION DEL STAFF MEDICO -->
+
+      <div class="card bg-purple ml-3 mr-3">
+        <div class="card-header">
+          <h3 class="card-title text-bold">STAFF MEDICO</h3>
+        </div>
+        <!-- /.card-header -->
+
+        <div class="card-body">
+          Médicos de primer nivel y los tecnólogos médicos más especializados, forman parte del staff de Clínica Topicos I, han sido capacitados en las más modernas aplicaciones que existen en el sector salud.
+        </div>
+        <!-- /.card-body -->
+        <a href="#" class="btn btn-secondary">BUSCAR MEDICO</a>
+      </div>
+      <!-- /.card -->
+
+
+      <!-- RESERVA TU CITA -->
+      <a href="#" class="btn btn-primary m-4 d-block ">RESERVA TU CITA</a>
+
+      <!-- MISION Y VISION -->
+      <div class="card bg-green ">
+        <div class="card-header">
+          <h3 class="card-title text-bold">MISION</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+          Formar lideres en ciencias, ingeniería y arquitectura, dotados de competencias para la investigación, innovación y gestión tecnológicas, capaces de contribuir al bienestar de la sociedad, al desarrollo del país y a la afirmación de nuestra identidad nacional.
+        </div>
+      </div>
+
+      <div class="card bg-green ">
+        <div class="card-header">
+          <h3 class="card-title text-bold">VISION</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+          Ser la Clínica líder en la creación de ciencias y tecnología, comprometida con el desarrollo sostenible de la Nación.
+        </div>
+      </div>
+
+      <!-- CENTRAL TELEFONICA -->
+      <div class="card  central-telefonica">
+        <div class="card-header border-0 d-flex justify-content-center">
+          <h3 class="card-title "><i class="fas fa-phone-volume"></i> Central telefonica</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body fa-3x d-flex justify-content-center">
+          +51 987654321
+        </div>
+      </div>
+
+
+      <!-- PROMOCIONES -->
+
+      <div class="container-fluid">
+        <h2 class="text-primary text-center mb-4">Promociones</h2>
+        <div class="row mt-4">
+
+          <div class="col-4 ">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/traumatologia.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Traumatologia</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/laboratorio.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Laboratorio</span>
+            </a>
+          </div>
+
+          <div class="col-4">
+            <a href="#" class="nav-link">
+              <img src="vistas/img/especialidades/covid.svg" class="mx-auto d-block" alt="">
+              <span class="d-block text-center">Covid-19</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+
+
+      <!-- NUESTRO CONVENIOS -->
+      <div class="card convenios">
+        <div class="card-header border-0">
+          <h3 class="card-title "><i class="fas fa-hands-helping"></i> Nuestros convenios</h3>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body fa-3x">
+          <div class="row">
+            <div class="col-6">
+              <img src="vistas/img/convenios/rimac.svg" class="mx-auto d-block" alt="">
+            </div>
+            <div class="col-6">
+              <img src="vistas/img/convenios/rimac.svg" class="mx-auto d-block" alt="">
+            </div>
+          </div>
+
+          <a href="#" class="btn btn-primary ">VER MAS CONVENIOS</a>
+        </div>
+      </div>
+
+
     </div>
     <!-- /.card-body -->
 
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -19,7 +19,7 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="vistas/dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <!-- <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet"> -->
 
   <link rel="stylesheet" href="vistas/css/estilos.css">
 
